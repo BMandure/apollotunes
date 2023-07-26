09dc64f847a650b9e79249849b50153044faddec
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-07-25 22:18:13 -0300

    fix play button at header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,8 +17,22 @@ function Header() {
         <div className="flex flex-col justify-between items-start p-5 [&]:font-semibold text-white">
           <small className="text-[1rem]">Category test</small>
           <h2 className="text-[5rem] leading-tight">Test list name</h2>
-          <button className="h-[70px] w-[70px] rounded-[50%] hover:border-2 hover:border-gray-600">
-            <img src={play} alt="..." />
+
+          <button
+            id="play"
+            className="w-[70px] h-[70px] border-transparent hover:border-2 hover:border-white rounded-[50%]"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-play-circle w-full h-full"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+            </svg>
           </button>
         </div>
       </div>
