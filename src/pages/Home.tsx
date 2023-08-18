42862e58db87a50857898617445a8dca62a5224f
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-08-17 22:51:01 -0300

    Add section 'My Playlist' at Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,8 +20,8 @@ function Home() {
       <p className="text-3xl">
         WELCOME to <span className="text-main">Apollotune</span>!
       </p>
-      <div id="playlists">
-        <p className="text-xl">Playlists:</p>
+      <div id="my-playlists">
+        <p className="text-xl">My Playlists:</p>
         <section className="flex gap-10 overflow-auto py-5">
           <MusicCard infoPlaylist={playlistTest} />
           <MusicCard infoPlaylist={playlistTest} />
@@ -58,6 +58,26 @@ function Home() {
           <MusicCard infoArtist={artistTest} />
           <MusicCard infoArtist={artistTest} />
           <MusicCard infoArtist={artistTest} />
+        </section>
+      </div>{" "}
+      <div id="playlists">
+        <p className="text-xl">Playlists:</p>
+        <section className="flex gap-10 overflow-auto py-5">
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
+          <MusicCard infoPlaylist={playlistTest} />
         </section>
       </div>
     </main>
