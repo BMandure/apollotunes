06daf2db2d9dcb1e45535d1e783b8120f8bde329
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-10-04 01:12:43 -0300

    Edit Playlist container to fix height

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,17 @@
 import SidebarButton from "./SidebarButton";
 import home from "../../assets/icons/home.svg";
 import fav from "../../assets/icons/fav.svg";
-import playlists from "../../assets/icons/playlists.svg";
 import logout from "../../assets/icons/logout.svg";
 import logo from "../../assets/logo.svg";
 
 function Sidebar() {
   return (
     <div className="h-full flex flex-col">
-      <span className="flex justify-start px-5 gap-2 cursor-default">
-        <img src={logo} alt="apollotunes_logo" className="w-[30px]" />
-        <h1 className="text-xl py-5">APOLLOTUNES</h1>
-      </span>
-      <ul className="[&>li]:p-2 flex-grow flex flex-col">
+      <ul className="[&>li]:p-2 flex flex-col h-full">
+        <li className="flex justify-start !ps-5 gap-2 cursor-default h-[8vh] items-center">
+          <img src={logo} alt="apollotunes_logo" className="w-[30px]" />
+          <h1 className="text-xl py-5">APOLLOTUNES</h1>
+        </li>
         <li>
           <SidebarButton text="Home" imgSrc={home} link="/" redButton={false} />
         </li>
@@ -24,12 +23,99 @@ function Sidebar() {
             redButton={false}
           />
         </li>
-        <li className="flex-grow relative">
-          <span className="w-full py-5 text-left px-3 rounded-xl hover:bg-zinc-700 flex items-center gap-2 h-[70px] pointer-events-none">
-            <img src={playlists} alt="sidebar_icon" className="w-[30px]" />
-            Playlists
-          </span>
-          <div className="flex flex-col overflow-auto h-[420px] px-1">
+        <li className="flex-grow overflow-y-auto my-3">
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+          <SidebarButton
+            text="Playlist Name"
+            imgSrc={logo}
+            link="/favourites"
+            redButton={false}
+          />
+        </li>
+        <li>
+          <SidebarButton
+            text="Logout"
+            imgSrc={logout}
+            link="/login"
+            redButton={true}
+          />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
+
+/*
+      <span className="flex justify-start px-5 gap-2 cursor-default">
+        <img src={logo} alt="apollotunes_logo" className="w-[30px]" />
+        <h1 className="text-xl py-5">APOLLOTUNES</h1>
+      </span>
+      <ul className="[&>li]:p-2 flex flex-col">
+        <li>
+          <SidebarButton text="Home" imgSrc={home} link="/" redButton={false} />
+        </li>
+        <li>
+          <SidebarButton
+            text="Favourite Songs"
+            imgSrc={fav}
+            link="/favourites"
+            redButton={false}
+          />
+        </li>
+      </ul>
+      <span className="w-[95%] h-[2px] mx-auto bg-zinc-800" />
+      <ul className="[&>li]:p-2 flex flex-col flex-1">
+        <li className="flex-grow relative !py-2 h-[53vh] overflow-y-auto">
+          <div className="flex flex-col px-1">
             <SidebarButton
               text="Playlist Name"
               imgSrc={logo}
@@ -80,7 +166,8 @@ function Sidebar() {
             />
           </div>
         </li>
-        <li className="mt-auto">
+        <span className="w-[95%] h-[2px] mx-auto bg-zinc-800" />
+        <li>
           <SidebarButton
             text="Logout"
             imgSrc={logout}
@@ -88,9 +175,4 @@ function Sidebar() {
             redButton={true}
           />
         </li>
-      </ul>
-    </div>
-  );
-}
-
-export default Sidebar;
+      </ul> */
